2b9065e1a5d32a56f78122105b5d41771b22421b
Claudio Parra <parraca@uci.edu>
2024-05-17 11:55:28 -0700

    papalyzer reverted to last satable state (before revisions to locality)

--- mapalyzer/instruments/locality.py
+++ mapalyzer/instruments/locality.py
@@ -2,152 +2,71 @@ import sys
 from collections import deque
 from .generic import GenericInstrument
 
-class TS:
-    def __init__(self, t, s):
-        self.t = t
-        self.s = s
-        return
 
-class CompactMap:
-    """Data Structure for fast access and manipulation of the MAP."""
-    time_size = 1
-    mem_size = 1
-    base_addr = 0
-    time_slice_size = 1
-    space_slice_size = 1
+class BufferedTrace:
+    cache_size = 64*64*512
+    block_size = 64
     def __init__(self):
-        self.time_space = [[] for _ in range(CompactMap.time_size)]
-        self.space_time = [[] for _ in range(CompactMap.mem_size)]
-        return
-    
+        # buffer with incoming accesses, and total bytes in it
+        self.buffer = deque()
+        self.buffer_bytes = 0
+        # locality of each window.
+        self.locality = []
+        # data ready to plot
+        self.Y = []
+
+
     def add_access(self, access):
-        for offset in range(access.size):
-            addr = access.addr - CompactMap.base_addr
-            time = access.time
-            self.time_space[time].append(addr+offset)
-            self.space_time[addr+offset].append(time)
-        return
-    
-    def get_spacially_sorted_time_window(self, time_last):
-        # get time window [t, s]
-        # (TODO: could be done by appending and trimming)
-        time_first = max(0, time_last+1 - CompactMap.time_slice_size)
-        win = []
-        win_len = 0
-        for t in range(time_last, time_first-1, -1):
-            for s in reversed(self.time_space[t]):
-                win.append((t,s))
-                win_len += 1
-                if win_len == CompactMap.time_slice_size:
-                    break
-            if win_len == CompactMap.time_slice_size:
-                break
-        win.sort(key=lambda x:(x[1],x[0])) # sort by space
-        print(f'vert win   [:{time_last:2}] ({len(win)}) {win}')
+        """Add a memory access to this thread's trace and create
+        a new window if there is enough accesses in the buffer."""
+        # append access to buffer
+        self.buffer.append(access)
+        win_id = access.time
+        self.buffer_bytes += access.size
+        # While there are enough bytes accessed in the buffer, create a window
+        while self.buffer_bytes > BufferedTrace.cache_size:
+            # trim buffer from the left until it fits in the cache
+            oldest_access = self.buffer.popleft()
+            self.buffer_bytes -= oldest_access.size
+        self.locality.append((win_id, self._win_dist_loc()))
 
-        # compute deltas [dt, ds]
-        deltas = win
-        for a,b,i in zip(win[:-1],win[1:],range(len(win)-1)):
-            deltas[i] = (abs(b[0]-a[0]), abs(b[1]-a[1]))
-        del deltas[-1]
-        print(f'vert deltas[:{time_last:2}] ({len(deltas)}) {deltas}')
-
-        # compute loc [lt, ls]
-        lt_fun = lambda dt: (CompactMap.time_slice_size - dt)\
-            /CompactMap.time_slice_size
-        ls_fun = lambda ds: (CompactMap.space_slice_size - ds)\
-            /CompactMap.space_slice_size
-        loc = [(lt_fun(dt),ls_fun(ds)) for dt,ds in deltas]
-        print(f'vert local [:{time_last:2}] ({len(loc)}) {loc}')
-        
-        x = input()
-        return win
-    
-    def get_temporally_sorted_space_window(self, space_last):
-        win = []
-        # get the left index of the window
-        space_first = max(0, space_last+1 - CompactMap.space_slice_size)
-        # get space window
-        for s in range(space_first, space_last+1):
-            for t in self.space_time[s]:
-                win.append((t,s))
-        # sort by time
-        win.sort(key=lambda x:(x[0],x[1]))
-        print(f'time win [:{space_last:2}] {win}')
-        return win
-
-# class BufferedTrace:
-#     cache_size = 64*64*512
-#     block_size = 64
-#     def __init__(self):
-#         # buffer with incoming accesses, and total bytes in it
-#         self.buffer = deque()
-#         self.buffer_bytes = 0
-#         # locality of each window.
-#         self.locality = []
-#         # data ready to plot
-#         self.Y = []
+    def _win_dist_loc(self):
+        """Compute the locality value for a window of memory accesses.
+        Bind the value to the last instruction of the window."""
+        # create window of accesses from the access buffer.
+        win_acc = []
+        for acc in self.buffer:
+            for off in range(acc.size):
+                win_acc.append(acc.addr+off)
+        win_acc = sorted(win_acc)
+        # compute neighbors distances within this window.
+        win_dis = [b-a for a,b in zip(win_acc[:-1],win_acc[1:])]
+        # compute locality based on neighbor distances.
+        loc_val = sum(max(0,
+                          (BufferedTrace.block_size - d) /
+                          (BufferedTrace.block_size * len(win_dis))
+                          )
+                      for d in win_dis
+                      )
+        # return tuple of instruction at the end of the window and locality
+        # value of this window.
+        return loc_val
 
 
-#     def add_access(self, access):
-#         """Add a memory access to this thread's trace and create
-#         a new window if there is enough accesses in the buffer."""
-#         # append access to buffer
-#         self.buffer.append(access)
-#         win_id = access.time
-#         self.buffer_bytes += access.size
-#         # While there are enough bytes accessed in the buffer, create a window
-#         while self.buffer_bytes > BufferedTrace.cache_size:
-#             # trim buffer from the left until it fits in the cache
-#             oldest_access = self.buffer.popleft()
-#             self.buffer_bytes -= oldest_access.size
-#         self.locality.append((win_id, self._win_dist_loc()))
-
-#     def _win_dist_loc(self):
-#         """Compute the locality value for a window of memory accesses.
-#         Bind the value to the last instruction of the window."""
-#         # create window of accesses from the access buffer.
-#         win_acc = []
-#         for acc in self.buffer:
-#             for off in range(acc.size):
-#                 win_acc.append(acc.addr+off)
-#         win_acc = sorted(win_acc)
-#         # compute neighbors distances within this window.
-#         win_dis = [b-a for a,b in zip(win_acc[:-1],win_acc[1:])]
-#         # compute locality based on neighbor distances.
-#         loc_val = sum(max(0,
-#                           (BufferedTrace.block_size - d) /
-#                           (BufferedTrace.block_size * len(win_dis))
-#                           )
-#                       for d in win_dis
-#                       )
-#         # return tuple of instruction at the end of the window and locality
-#         # value of this window.
-#         return loc_val
-
-
-#     def create_plotable_data(self, X):
-#         # fill potential gaps while creating the Y array.
-#         # The Y array ranges from 0 to 100.
-#         self.Y = [0] * len(X)
-#         for instr_id,val in self.locality:
-#             self.Y[instr_id] = 100 * val
-
+    def create_plotable_data(self, X):
+        # fill potential gaps while creating the Y array.
+        # The Y array ranges from 0 to 100.
+        self.Y = [0] * len(X)
+        for instr_id,val in self.locality:
+            self.Y[instr_id] = 100 * val
 
 
 class Locality(GenericInstrument):
     def __init__(self, instr_counter, cache_size, block_size, map_metadata,
                  verb=False):
         super().__init__(instr_counter, verb=False)
-        #BufferedTrace.cache_size = cache_size
-        #BufferedTrace.block_size = block_size
-
-        # Setup compact map class
-        CompactMap.mem_size = map_metadata.mem_size
-        CompactMap.base_addr = map_metadata.base_addr
-        CompactMap.time_size = map_metadata.time_size
-        CompactMap.time_slice_size = cache_size
-        CompactMap.space_slice_size = block_size
+        BufferedTrace.cache_size = cache_size
+        BufferedTrace.block_size = block_size
         
         # each thread has its own access trace.
         self.buffer_traces = {}
@@ -162,19 +81,11 @@ class Locality(GenericInstrument):
         self.plot_color_fill  = '#A2106511' # Fuchsia semi transparent
         self.plot_color_bg    = '#FFFFFF00' # transparent
 
-
-    def register_access_DEPRECATED(self, access):
-        if not self.enabled:
-            return
-        if access.thread not in self.buffer_traces:
-            self.buffer_traces[access.thread] = BufferedTrace()
-        self.buffer_traces[access.thread].add_access(access)
-        
     def register_access(self, access):
         if not self.enabled:
             return
         if access.thread not in self.buffer_traces:
-            self.buffer_traces[access.thread] = CompactMap()
+            self.buffer_traces[access.thread] = BufferedTrace()
         self.buffer_traces[access.thread].add_access(access)
 
     def get_extent(self):
@@ -188,19 +99,6 @@ class Locality(GenericInstrument):
         return extent
 
     def plot(self, axes, basename='locality', extent=None):
-        for thread in self.buffer_traces:
-            for i in range(CompactMap.time_size):
-                twin = self.buffer_traces[thread].\
-                    get_spacially_sorted_time_window(i)
-            print()
-            for i in range(CompactMap.mem_size):
-                swin = self.buffer_traces[thread].\
-                    get_temporally_sorted_space_window(i)
-                print(f'spac win [:{i:2}]: {swin}')
-
-        exit(0)
-
-        
         for thread in self.buffer_traces:
             self.buffer_traces[thread].create_plotable_data(self.X)
 

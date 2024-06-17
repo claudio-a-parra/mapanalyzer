8f7128defe412ddc42b3a944178d5da6d9a1c0a7
Claudio Parra <parraca@uci.edu>
2024-06-17 11:44:01 -0700

    no memory check

--- mapalyzer/settings.py
+++ mapalyzer/settings.py
@@ -207,11 +207,12 @@ class MapSpecs:
             sys.exit(1)
 
         # error if start_addr, mem_size and end_addr are incoherent.
-        if self.end_addr - self.start_addr + 1 != self.mem_size:
-            print(f'Error: The size, start and end address of the memory '
-                  f'declared is incoherent in {self.file_path}.')
-            print(self)
-            sys.exit(1)
+        # if self.end_addr - self.start_addr + 1 != self.mem_size:
+        #     print(f'Error: The size, start and end address of the memory '
+        #           f'declared is incoherent in {self.file_path}.')
+        #     print(self)
+        #     sys.exit(1)
+        print('[!] DBG: the memory start, end, and size check is disabled (settings.py:215)')
 
         # convert max-time index (what comes in the file) to time_size
         self.time_size +=1

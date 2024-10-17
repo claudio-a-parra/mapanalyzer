d92a29a103e9c86aa129cb2212143dbb12a3a871
Claudio Parra <parraca@uci.edu>
2024-10-16 17:36:01 -0700

    ch03: review example convergent random access

--- mem_tracer/mem_tracer.cpp
+++ mem_tracer/mem_tracer.cpp
@@ -68,7 +68,7 @@ The idea is to avoid dynamic allocation. Why? because we are measuring real
 time events and if we go down to the OS to request memory at runtime, well...
 we fuck up all the measurments. */
 const UINT16 MAX_THREADS = 32;
-const UINT32 MAX_THR_EVENTS = 900000000;
+const UINT32 MAX_THR_EVENTS = 900000000; // 900,000,000 * 192 bytes =
 std::stringstream metadata;
 std::stringstream data;
 std::stringstream error;
